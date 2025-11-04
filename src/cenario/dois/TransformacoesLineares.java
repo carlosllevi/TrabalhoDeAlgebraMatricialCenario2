c666fb505d6cc18b0cdecbfc2a9307b5446f3798
carloslevi <carlos2levi@gmail.com>
2025-11-04 07:41:21 -0300

    ultima att

--- src/cenario/dois/TransformacoesLineares.java
+++ src/cenario/dois/TransformacoesLineares.java
@@ -30,14 +30,22 @@ public class TransformacoesLineares {
 
     public static Vector rotation3DY(Vector v, double angle) {
         Vector v_hom = new Vector(4, new double[]{v.getDouble(0), v.getDouble(1), v.getDouble(2), 1.0});
-        Matrix m = new Matrix(4,4, new double[]{Math.cos(angle),0,Math.sin(angle),0,0,1,0,0,(-1* Math.sin(angle)), 0, (Math.cos(angle)),0,0,0,0,1});
+        Matrix m = new Matrix(4,4, new double[]{
+                Math.cos(angle),    0,  Math.sin(angle),    0,
+                0,                  1,          0,          0,
+                (-1* Math.sin(angle)), 0, (Math.cos(angle)),0,
+                0,                     0,        0,         1});
         Vector resultado = LinearAlgebra.dotMV(m,v_hom);
         return new Vector(3, new double[] {Math.round(resultado.getDouble(0)), Math.round(resultado.getDouble(1)), Math.round(resultado.getDouble(2))});
     }
 
     public static Vector rotation3DZ(Vector v, double angle) {
         Vector v_hom = new Vector(4, new double[]{v.getDouble(0), v.getDouble(1), v.getDouble(2), 1.0});
-        Matrix m = new Matrix(4,4, new double[]{Math.cos(angle),(-1* Math.sin(angle)), 0,0, Math.sin(angle),Math.cos(angle),0,0,0,0, 1,0,0,0,0,1});
+        Matrix m = new Matrix(4,4, new double[]{
+                Math.cos(angle),    (-1* Math.sin(angle)),  0,   0,
+                Math.sin(angle),Math.cos(angle),    0,           0,
+                0,             0,                   1,           0,
+                0,             0,                   0,           1});
         Vector resultado = LinearAlgebra.dotMV(m,v_hom);
         return new Vector(3, new double[] {Math.round(resultado.getDouble(0)), Math.round(resultado.getDouble(1)), Math.round(resultado.getDouble(2))});
     }
